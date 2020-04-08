805f3afbb64a39df0b630a15bb2a4d399de3a3db
Malec <andrey193728@yandex.ru>
2020-04-08 13:21:19 +0300

    create login activity markup

--- app/src/main/java/com/proj/memeboard/SplashActivity.kt
+++ app/src/main/java/com/proj/memeboard/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import androidx.core.os.postDelayed
+import com.proj.memeboard.login.LoginActivity
 
 class SplashActivity: AppCompatActivity() {
     private val splashScreenDuration = 300L
@@ -14,7 +14,7 @@ class SplashActivity: AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, splashScreenDuration)
     }
