05f79438131bba4d1885324e61bc5f52eb161368
Malec <andrey193728@yandex.ru>
2020-04-08 16:47:45 +0300

    add ui logic to login activity

--- app/src/main/java/com/proj/memeboard/login/LoginActivity.kt
+++ app/src/main/java/com/proj/memeboard/login/LoginActivity.kt
@@ -1,12 +1,58 @@
 package com.proj.memeboard.login
 
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
 import com.proj.memeboard.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginEditText.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+
+        loginEditText.setOnFocusChangeListener { _, hasFocus ->
+            loginLayout.error = if (hasFocus) loginLayout.error else null
+        }
+
+        passEditText.doAfterTextChanged {
+            passLayout.helperText = if (passEditText.text.isNullOrBlank()) getString(R.string.password_helper) else null
+        }
+
+        passEditText.setOnFocusChangeListener { _, hasFocus ->
+            passLayout.helperText = if (hasFocus && passEditText.text.isNullOrBlank()) getString(R.string.password_helper) else null
+        }
+
+        progressBar.indeterminateDrawable.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
+
+        loginButton.setOnClickListener {
+            if (!hasInputErrors()) return@setOnClickListener
+
+            showProgress()
+        }
+    }
+
+    private fun showProgress() {
+        loginButton.text = ""
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgress() {
+        loginButton.text = getString(R.string.sign_in)
+        progressBar.visibility = View.GONE
+    }
+
+    private fun hasInputErrors(): Boolean {
+        val blankInputError = getString(R.string.blank_input_error)
+        loginLayout.error = if (loginEditText.text.isNullOrBlank()) blankInputError else null
+        passLayout.error = if (passEditText.text.isNullOrBlank()) blankInputError else null
+
+        return loginLayout.error == null && passLayout.error == null
     }
 }
---
+++ app/src/main/java/com/proj/memeboard/login/LoginViewModel.kt
@@ -0,0 +1,4 @@
+package com.proj.memeboard.login
+
+class LoginViewModel {
+}
